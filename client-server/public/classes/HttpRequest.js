5cc816072c0cb8e7601c23b8837b1f4a89bff9d1
Arthur Tavares <arthurabreu00@gmail.com>
2018-10-12 22:33:25 -0300

    Finalização do projeto

--- client-server/public/classes/HttpRequest.js
+++ client-server/public/classes/HttpRequest.js
@@ -1,66 +1,68 @@
 class HttpRequest {
 
-    static get(url,params = {}){
+    static get(url, params = {}){
 
-        HttpRequest.request('GET',url,params);
-
-    }
-
-    static delete(url,params = {}){
-
-        HttpRequest.request('DELETE',url,params);
+        return HttpRequest.request('GET', url, params);
 
     }
 
+    static delete(url, params = {}){
 
-    static put(url,params = {}){
-
-        HttpRequest.request('PUT',url,params);
-
-    }
-
-    static post(url,params = {}){
-
-        HttpRequest.request('POST',url,params);
+        return HttpRequest.request('DELETE', url, params);
 
     }
 
+    static put(url, params = {}){
 
-    static request(method,url,params = {}) {
+        return HttpRequest.request('PUT', url, params);
 
-        return new Promise((resolve,reject)=>{
+    }
 
-            let ajax = new XMLHttpRequest;
+    static post(url, params = {}){
 
-            ajax.open(method.toUpperCase(),url);
-    
-            ajax.onerror = event =>{
-                
+        return HttpRequest.request('POST', url, params);
+
+    }
+
+    static request(method, url, params = {}){
+
+        return new Promise((resolve, reject) => {
+
+            let ajax = new XMLHttpRequest();
+
+            ajax.open(method.toUpperCase(), url);
+
+            ajax.onerror = event => {
+
                 reject(e);
 
             }
 
             ajax.onload = event => {
-    
-                let obj = {}
-                
+
+                let obj = {};
+
                 try {
+
                     obj = JSON.parse(ajax.responseText);
 
-                } catch (e) {
-                    
-                    reject(e)
-                    console.log(e);
+                } catch(e){
+
+                    reject(e);
+                    console.error(e);
+
                 }
 
-                resolve(obj)
-    
-    
+                resolve(obj);
+            
             };
-    
-            ajax.send();
+
+            ajax.setRequestHeader('Content-Type', 'application/json');
+
+            ajax.send(JSON.stringify(params));
 
         });
 
     }
+
 }
